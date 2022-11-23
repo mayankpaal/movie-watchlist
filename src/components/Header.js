f8ce64e83d6319fb00c1e902c4152a133b144c77
Mayank Paal <mayankpaal20@gmail.com>
2022-11-23 15:38:18 +0530

    implete basic react watchlist webapp with react

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+          <div className="brand">
+            <Link to="/">WatchList</Link>
+          </div>
+
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Watch List</Link>
+            </li>
+
+            <li>
+              <Link to="/watched">Watched</Link>
+            </li>
+
+            <li>
+              <Link to="/add" className="btn btn-main">
+                + Add
+              </Link>
+            </li>
+
+            <li>
+              <Link to='/welcome' className="btn btn-main">
+                Get Started
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/config/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJnjOuGkiyV87QYJAU5O6dv04niHoTAHs",
+  authDomain: "movie-search-d730d.firebaseapp.com",
+  projectId: "movie-search-d730d",
+  storageBucket: "movie-search-d730d.appspot.com",
+  messagingSenderId: "55045164840",
+  appId: "1:55045164840:web:5d441d2432d8c6ea7b8a1d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
